7ac10c456af05fce074bbfad6844c12d217ee9fb
ThomasLavallee <lavallee@udel.edu>
2025-03-05 18:05:25 -0500

    Finished RevealAnswer component and fixed Counter

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,21 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    // Get the state and state setter
+    const [isVisible, visibilitySetter] = useState<boolean>(false);
+    const answer: number = 42;
+
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    visibilitySetter(!isVisible);
+                }}
+            >
+                Reveal Answer
+            </Button>
+            {isVisible ? <span>{answer}</span> : <span></span>}
+            Reveal Answer
+        </div>
+    );
 }
